972e1c8abaeed84fad958331f901442dd4e62848
zeratheuz <zeratheuz@gmail.com>
2025-09-11 11:50:26 -0300

    Update CSS and JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles.css"
 
 const divContent = document.querySelector("#content")
+const divWelcome = document.createElement("div")
 const divName = document.createElement("div")
-divName.textContent = `Welcome to Ichiraku Ramen! 一楽ラーメン`
+divWelcome.textContent = `Welcome to Ichiraku Ramen!`
+divName.textContent = `一楽ラーメン`
+divContent.appendChild(divWelcome)
 divContent.appendChild(divName)
 
 console.log("Welcome to 一楽ラーメン")
