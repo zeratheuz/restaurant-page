5888890848a84ef6984365ec229bca51fdbf6a20
zeratheuz <zeratheuz@gmail.com>
2025-09-11 15:51:30 -0300

    Add JS modules for home, menu, about

---
+++ src/home.js
@@ -0,0 +1,12 @@
+export function home(divContent) {
+  divContent.innerHTML = ''
+
+  const divWelcome = document.createElement("div")
+  const divName = document.createElement("div")
+  
+  divWelcome.textContent = `Welcome to Ichiraku Ramen!`
+  divName.textContent = `一楽ラーメン`
+  
+  divContent.appendChild(divWelcome)
+  divContent.appendChild(divName)
+} 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import "./styles.css"
+import {home} from "./home.js"
+// import home from "./menu.js"
+// import home from "./about.js"
 
 const divContent = document.querySelector("#content")
-const divWelcome = document.createElement("div")
-const divName = document.createElement("div")
-divWelcome.textContent = `Welcome to Ichiraku Ramen!`
-divName.textContent = `一楽ラーメン`
-divContent.appendChild(divWelcome)
-divContent.appendChild(divName)
+const homeButton = document.querySelector("#home")
+const menuButton = document.querySelector("#menu")
+const aboutButton = document.querySelector("#about")
+
+homeButton.addEventListener("click", () => home(divContent))
+home(divContent)
+
+menu.addEventListener("click", () => {
+  divContent.innerHTML = ''  
+  const divWelcome = document.createElement("div")
+  const divName = document.createElement("div")
+  divWelcome.textContent = `Ramen!`
+  divName.textContent = `一楽ラーメン`
+  divContent.appendChild(divWelcome)
+  divContent.appendChild(divName)
+})
+
+about.addEventListener("click", () => {
+  divContent.innerHTML = ''  
+  const divWelcome = document.createElement("div")
+  const divName = document.createElement("div")
+  divWelcome.textContent = `Ohayoooooooooo!`
+  divName.textContent = `一楽ラーメン`
+  divContent.appendChild(divWelcome)
+  divContent.appendChild(divName)
+})
 
 console.log("Welcome to 一楽ラーメン")
