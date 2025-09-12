cc3a5b18449fac9ca87f5d447c28ceaaf009d1b6
zeratheuz <zeratheuz@gmail.com>
2025-09-12 16:46:48 -0300

    Update menu.js content

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,29 @@
-import ichirakuRamenWithMisoTonkotsuSoup from "src/IchirakuRamenwithMisoTonkotsuSoup.webp"
+import ichirakuRamenWithMisoTonkotsuSoup from "./IchirakuRamenwithMisoTonkotsuSoup.webp"
 export function menu(divContent) {
   divContent.innerHTML = ''
 
-  const divWelcome = document.createElement("div")
+  const divMenu = document.createElement("div")
   const divMessage = document.createElement("div")
   const divName = document.createElement("div")
 
-  divWelcome.classList.add("decoration")
+  divMenu.classList.add("decoration")
   
   divName.textContent = `一楽ラーメン`
   divMessage.textContent = `Menu:`
+
+  divMenu.appendChild(divName)
+  divMenu.appendChild(divMessage)
+  
+  const divDishName = document.createElement("div")
+  divDishName.classList.add("text")
+  divDishName.textContent = "Ichiraku Ramen with Miso Tonkotsu Soup:"
 
   const imgRamenMisoTonkotsuSoup = document.createElement("img")
   imgRamenMisoTonkotsuSoup.src = ichirakuRamenWithMisoTonkotsuSoup
   imgRamenMisoTonkotsuSoup.alt = "Ichiraku Ramen With Miso Tonkotsu Soup"
   
-  divWelcome.appendChild(divName)
-  divWelcome.appendChild(divMessage)
+  divMenu.appendChild(divDishName)
+  divMenu.appendChild(imgRamenMisoTonkotsuSoup)
 
-  divContent.appendChild(divWelcome)
+  divContent.appendChild(divMenu)
 } 
