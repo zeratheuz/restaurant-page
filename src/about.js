c16a40676b3ddb109036081878c95f0a87cba951
zeratheuz <zeratheuz@gmail.com>
2025-09-15 10:45:00 -0300

    Update CSS and JS

--- src/about.js
+++ src/about.js
@@ -8,12 +8,25 @@ export function about(divContent) {
   const divName = document.createElement("div")
 
   divWelcome.classList.add("decoration")
-  
+  divMessage.classList.add("title")
+
   divMessage.textContent = `About Ichiraku Ramen!`
   divName.textContent = `一楽ラーメン`
-  
+
   divWelcome.appendChild(divName)
   divWelcome.appendChild(divMessage)
+
+  const imgIchirakuRamen = document.createElement("img")
+  const divText = document.createElement("div")
+
+  imgIchirakuRamen.src = ichirakuRamenTeuchi
+  imgIchirakuRamen.alt = "Ichiraku Ramen Teuchi"
+  divText.textContent = `Founded decades ago, Ichiraku Ramen is the meeting spot for ramen lovers in Konoha.  
+With a secret recipe passed down through generations, our ramen brings flavor and tradition in every bowl.`
+
+  divWelcome.appendChild(imgIchirakuRamen)
+  divWelcome.appendChild(divText)
+
 
   divContent.appendChild(divWelcome)
 } 
